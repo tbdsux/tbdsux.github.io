697f8c7582619ff9e0c13a35646738d241cafa0b
Joshue Abance <iamcoderx@gmail.com>
2023-09-10 16:50:38 +0800

    chore: update astro to `v3`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), preact(), mdx()],
+  integrations: [tailwind(), preact(), mdx()],
   site: "https://tbdsux.dev",
   compressHTML: true,
 });
